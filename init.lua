61747f60a085a9749f326038e3ab5255c58067f3
Harry Parkes <harrydparkes@proton.me>
2024-06-09 15:00:37 +1200

    spelling fixed in init.lua

--- init.lua
+++ init.lua
@@ -143,12 +143,12 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  -- NOTE: Plugins can be added with a link (or for a GitHub repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
+  -- keys can be used to configure plugin behaviour/loading/etc.
   --
   -- Use `opts = {}` to force a plugin to be loaded.
   --

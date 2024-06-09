4e30561e97c763bb6d6df7f096d2fefe7f936027
Harry Parkes <harrydparkes@proton.me>
2024-06-09 15:13:53 +1200

    Added rst docstring injection to class and module docstrings

--- after/queries/python/injections.scm
+++ after/queries/python/injections.scm
@@ -1,5 +1,8 @@
 ;; extends
+
 ; This file tells TreeSitter to parse Python docstrings with the rst parser
+
+; Function docstrings
 ((function_definition
   body: (block .
     (expression_statement
@@ -9,4 +12,25 @@
     )
   )
 ) (#set! injection.language "rst"))
+
+; Class docstrings
+((class_definition
+  body: (block .
+    (expression_statement
+      (string
+        (string_content) @injection.content
+      )
+    )
+  )
+) (#set! injection.language "rst"))
+
+; Module docstrings
+((module .
+  (expression_statement
+    (string
+      (string_content) @injection.content
+    )
+  )
+) (#set! injection.language "rst"))
+
 

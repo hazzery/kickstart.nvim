41fa13f37b676e56062945fa0103d831d5e97188
Harry Parkes <harrydparkes@proton.me>
2024-06-12 08:22:12 +1200

    changed branch of pyvenv as it was complaining about upgrading

--- init.lua
+++ init.lua
@@ -182,6 +182,7 @@ require('lazy').setup({
 
   {
     'linux-cultist/venv-selector.nvim',
+    branch = 'regexp',
     dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
     opts = {
       name = 'venv',
